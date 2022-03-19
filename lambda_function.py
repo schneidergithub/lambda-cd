728282428ea2dfa8744c62aeedc06e8ccef236fd
Aaron Schneider <contact@aaronschneider.org>
2022-03-19 12:06:08 -0600

    new name

---
+++ lambda_function.py
@@ -0,0 +1,20 @@
+import json
+
+print('loading function')
+
+def lambda_handler(event, context):
+    #transactionName = event['queryStringParameters']['name']
+
+    transactionName = "testcommit4"
+    print('transactionName=' + transactionName)
+
+    transactionResponse = {}
+    transactionResponse['transactionName'] = transactionName
+    
+    responseObject = {}
+    responseObject['statusCode'] = 200
+    responseObject['headers'] = {}
+    responseObject['headers']['Content-Type'] = 'application/json'
+    responseObject['body'] = json.dumps(transactionResponse)
+
+    return responseObject
