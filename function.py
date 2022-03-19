334d5fb78fb39e0b24c2fc683a799b14fee7aacd
Aaron Schneider <contact@aaronschneider.org>
2022-03-19 11:29:25 -0600

    test commit 2

--- function.py
+++ function.py
@@ -5,7 +5,7 @@ print('loading function')
 def lambda_handler(event, context):
     #transactionName = event['queryStringParameters']['name']
 
-    transactionName = "testcommit"
+    transactionName = "testcommit2"
     print('transactionName=' + transactionName)
 
     transactionResponse = {}
