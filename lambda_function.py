f36eadbe168c98253c57e2a065956f2a4e05ba6f
Aaron Schneider <contact@aaronschneider.org>
2022-03-20 09:07:06 -0600

    test environment rules

--- lambda_function.py
+++ lambda_function.py
@@ -5,7 +5,7 @@ print('loading function')
 def lambda_handler(event, context):
     #transactionName = event['queryStringParameters']['name']
 
-    transactionName = "test commit 7"
+    transactionName = "test commit 8"
     print('transactionName=' + transactionName)
 
     transactionResponse = {}
